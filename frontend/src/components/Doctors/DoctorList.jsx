00510ec7c3776339bdf94736cac362bf334e93a2
gg_ <89791105+ggvhs@users.noreply.github.com>
2023-10-16 11:26:45 -0400

    hard coded data for doctor list added to doctor page

--- frontend/src/components/Doctors/DoctorList.jsx
+++ frontend/src/components/Doctors/DoctorList.jsx
@@ -7,9 +7,9 @@ const DoctorList = () => {
 return (
 <div className="grid grid-cols-1 sm:grid-cold-2 md:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]">
     {doctors.map(doctor => (
-        <DoctorCard key={doctor.id} doctor={doctor} />
+    <DoctorCard key={doctor.id} doctor={doctor} />
     ))}
-    </div>
+</div>
 );
 };
 
--- frontend/src/pages/Doctors/Doctors.jsx
+++ frontend/src/pages/Doctors/Doctors.jsx
@@ -4,6 +4,7 @@ import { doctors } from './../../assets/data/doctors'
 
 const Doctors = () => {
   return (
+    <>
     <section className='bg-[#fff9ea]'>
       <div className='container text-center'>
         <h2 className='heading'> Find a Doctor</h2>
@@ -11,9 +12,26 @@ const Doctors = () => {
           <input type="search" className='py-4 pl-4 pr-2 bg-transparent w-full 
           focus:outline-none cursor-ponter placeholder:text-textColor' 
           placeholder='Search doctors' />
+          <button className='btn mt-0 rounded-[0px] rounded-r-md'>
+            Search
+          </button>
         </div>
       </div>
     </section>
+
+    <section>
+      <div className='container'>
+        <div className="grid grid-cols-1 sm:grid-cold-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+          {doctors.map(doctor => (
+          <DoctorCard key={doctor.id} doctor={doctor} />
+        ))}
+        </div>
+
+      </div>
+    </section>
+    </>
+
+    
   )
 }
 
