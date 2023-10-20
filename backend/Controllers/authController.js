b87a74c2bd6aaf255281b7f1fb8b5c78689d22aa
gg_ <89791105+ggvhs@users.noreply.github.com>
2023-10-20 10:16:01 -0400

    routes added/ authController added

---
+++ backend/Controllers/authController.js
@@ -0,0 +1,16 @@
+export const register = async(req,res) =>{
+
+        const {} = req.body
+    try {
+        
+    } catch (err) {
+        
+    }
+}
+export const login = async(req,res) =>{
+    try {
+        
+    } catch (err) {
+        
+    }
+}
